b733415f0c73f88ef7fc2c02a38362f41936818c
Kyle Hickinson <git@kylehickinson.com>
2022-06-22 22:23:39 -0400

    Fix build for iOS 16/macOS 13

--- Sources/WebView/WebView.swift
+++ Sources/WebView/WebView.swift
@@ -34,15 +34,23 @@ public class WebViewStore: ObservableObject {
       subscriber(for: \.canGoBack),
       subscriber(for: \.canGoForward)
     ]
-    if #available(iOS 15.0, *) {
+    if #available(iOS 15.0, macOS 12.0, *) {
       observers += [
         subscriber(for: \.themeColor),
         subscriber(for: \.underPageBackgroundColor),
-        subscriber(for: \.fullscreenState),
         subscriber(for: \.microphoneCaptureState),
         subscriber(for: \.cameraCaptureState)
       ]
     }
+#if swift(>=5.7)
+    if #available(iOS 16.0, macOS 13.0, *) {
+      observers.append(subscriber(for: \.fullscreenState))
+    }
+#else
+    if #available(iOS 15.0, macOS 12.0, *) {
+      observers.append(subscriber(for: \.fullscreenState))
+    }
+#endif
   }
   
   private var observers: [NSKeyValueObservation] = []
